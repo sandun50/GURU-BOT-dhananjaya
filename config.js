5f67aebd4878f865aa08c82d9b06f7c51f96b55e
sandun50 <133552977+sandun50@users.noreply.github.com>
2023-05-14 08:52:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['94783598811', 'sandun', true],
+  ['94783598811', 'Ash', true], 
   [''] 
 ] //Number of owners
 
